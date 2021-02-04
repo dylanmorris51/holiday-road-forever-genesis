35cb37773e285ecda3f85596f53f03c92a173b4a
emilycentko <emily.centko@gmail.com>
2021-02-04 09:16:28 -0600

    Registered and applied API keys

--- scripts/.Settings.js
+++ scripts/.Settings.js
@@ -1,5 +1,5 @@
 export const settings = {
 	graphhopperKey: "",
-	npsKey: "ah8dBq6bQqgUUcPyhQKofK4erY9syzJstzKHMGMD",
-	weatherKey: "a56ce011c11186efab6be61f90341659"
+	npsKey: "",
+	weatherKey: ""
 };
