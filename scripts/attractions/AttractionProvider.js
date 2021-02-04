38c8484f88546bcc1fff22fe6aae444f020948bc
Thomas Witherow <twitherow@bellsouth.net>
2021-02-04 11:10:56 -0600

    attractions api

---
+++ scripts/attractions/AttractionProvider.js
@@ -0,0 +1,20 @@
+let attractions = []
+
+
+
+ export const useAttractions = () => {
+     return attractions.slice()
+ }
+  export const getAttractions = () => {
+     return fetch("http://holidayroad.nss.team/bizarreries")
+     .then(response => response.json())
+     .then(
+         parsedAttractions => {
+             console.log("parks", parsedAttractions)
+             attractions = parsedAttractions.data
+         }
+     )
+  }
+    
+
+ 
