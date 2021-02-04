d1a397f4dd45e6d2af4e5fc7c0930f9dffd72481
Dylan Morris <dylanrobertmorris@gmail.com>
2021-02-04 15:31:39 -0600

    made event listeners on the park select dropdown to dispatch the park seleceted id for the parkList component and the park object lat/lon coordinates to be dispatched to and then used by the weathlist.js component

--- scripts/parks/ParkSelect.js
+++ scripts/parks/ParkSelect.js
@@ -3,18 +3,53 @@ import { useParks, getParks } from './ParkProvider.js'
 const eventHub = document.querySelector(".container")
 const contentTarget = document.querySelector(".aside__left")
 
+// Event for ParkSelect dropdown ==> ParkList.js
 eventHub.addEventListener("change", event => {
     if(event.target.id==="parkSelect") {
+        
         const customEvent = new CustomEvent("parkChosen", {
             detail: {
                 parkThatWasChosen: event.target.value
+                
             }
         })
+        console.log("Park That Was Chosen:", customEvent.detail.parkThatWasChosen)
         eventHub.dispatchEvent(customEvent)
         console.log("Park Selection Event Successfully Dispatched!")
     }
 })
 
+
+
+
+
+// Event for ParkSelect dropdown ==> WeatherList.js (sending the right coordinates)
+eventHub.addEventListener("change", changeEvent => {
+    if(changeEvent.target.id==="parkSelect") {
+        getParks()
+            .then(() => {
+                const parklocation = useParks()
+                // Filter for coordinates
+                const selectedPark = changeEvent.target.value
+                console.log('selectedPark: ', selectedPark);
+                const parkObject = parklocation.find(park => park.id === selectedPark)
+                console.log('parkObject: ', parkObject);
+                const dataPayload = {
+                    lon: parkObject.longitude,
+                    lat: parkObject.latitude
+                }
+                console.log('dataPayload: ', dataPayload);
+                
+                const coordinatesEvent = new CustomEvent("coordinates", {
+                    detail: {
+                        coordinates: dataPayload
+                    }
+                })
+                eventHub.dispatchEvent(coordinatesEvent)
+                console.log("Coordinates Were Succesfully Dispatched!", coordinatesEvent.detail.coordinates)
+            })
+    }
+})
 
 const render = parkSelection => {
     contentTarget.innerHTML = `
