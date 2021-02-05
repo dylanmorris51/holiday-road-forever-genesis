1d461c4a0196b9adb4f92b2c2070d0c378c8f93d
nicoletatum <nicoletatum@outlook.com>
2021-02-05 13:33:20 -0600

    fixed console.table for dylan and then fixed dot notation name to be correct in eatery.js

--- scripts/eateries/Eatery.js
+++ scripts/eateries/Eatery.js
@@ -1,6 +1,6 @@
 export const Eatery = (eateryObj) => {
     return `<section class="preview__card eatery__card">
-    <h3 class="eatery__name">${eateryObj.fullName}</h3>
+    <h3 class="eatery__name">${eateryObj.businessName}</h3>
     <img class="eatery__img" src="" alt="">
     <button id="eateryDetails">Details</button>
 </section>`
