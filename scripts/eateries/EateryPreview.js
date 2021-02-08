f69011c28da39b6343be48862339ab23c0b9ef94
emilycentko <emily.centko@gmail.com>
2021-02-08 14:06:54 -0600

    Create Details button on Eateries Preview to show Details modal with more info about selected eatery

--- scripts/eateries/EateryPreview.js
+++ scripts/eateries/EateryPreview.js
@@ -1,10 +1,12 @@
 import { useEateries } from './EateryProvider.js'
+import { ShowEateryButton } from './EateryButton.js'
 import './EaterySelect.js'
 
 
 const eventHub = document.querySelector(".container")
 const contentTarget = document.querySelector(".eatery__card")
 
+// eatery selected event from dropdown to render on Preview
 
 eventHub.addEventListener("eateryChosen", eateryChosenEvent => {
     if (eateryChosenEvent.detail.eateryChosen !== "0") {
@@ -20,6 +22,49 @@ eventHub.addEventListener("eateryChosen", eateryChosenEvent => {
 const renderEatery = (eateryObj) => {
 
     contentTarget.innerHTML = `
-    <div class="eatery_name">${eateryObj.businessName}</div>
+    <div class="eatery_name">${eateryObj.businessName}
+    ${ShowEateryButton(eateryObj)}
+    </div>
     `
 }
+
+// details button modal HTML
+
+const contentContainer = document.querySelector(".modal__card")
+
+export const EateryModal = (eateryObj) => {
+    const EateryHTMLRepresentation = 
+        `<div id="eatery_modal" class="modal_parent">
+            <div class="modal__content">
+                <h2>More Information</h2>
+                <h3 class="eatery__name">${eateryObj.businessName}</h3>
+                <section class="modal__info">
+                <div class="eatery__description">${eateryObj.description}</div>
+                </section>
+                <button id="modal__close">Close</button>    
+            </div>
+        </div>`
+
+    contentContainer.innerHTML = EateryHTMLRepresentation
+}
+
+// click event for details button
+
+eventHub.addEventListener("detailsClicked", event => {
+    console.log("event works idk", event.detail.eateryId)
+
+    const selectedEateryId = event.detail.eateryId
+    const eateriesArray = useEateries()
+    const eaterySelection = eateriesArray.find((eateryObj) => eateryObj.id === selectedEateryId)
+    EateryModal(eaterySelection)
+})
+
+eventHub.addEventListener("click", event => {
+    if (event.target.id === "modal__close") {
+        closeModal()
+    }
+})
+
+const closeModal = () => {
+    contentContainer.innerHTML = ""
+    }
