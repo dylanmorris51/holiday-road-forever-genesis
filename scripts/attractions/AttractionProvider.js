9cfe837252da0b05a0a213ab9348eb61c75e6e5e
Dylan Morris <dylanrobertmorris@gmail.com>
2021-02-08 14:51:44 -0600

    initialized a details button in for the national parks that displays the park description and directions in a pop up modal

--- scripts/attractions/AttractionProvider.js
+++ scripts/attractions/AttractionProvider.js
@@ -3,16 +3,16 @@ let attractions = []
 
 
 
- export const useAttractions = () => {
-     return attractions.slice()
- }
-  export const getAttractions = () => {
-     return fetch("http://holidayroad.nss.team/bizarreries")
-     .then(response => response.json())
-     .then(
-         parsedAttractions => {
-             console.log("attractions", parsedAttractions)
-             attractions = parsedAttractions
-         }
-     )
-  }
+export const useAttractions = () => {
+    return attractions.slice()
+}
+export const getAttractions = () => {
+    return fetch("http://holidayroad.nss.team/bizarreries")
+        .then(response => response.json())
+        .then(
+            parsedAttractions => {
+                console.log("attractions", parsedAttractions)
+                attractions = parsedAttractions
+            }
+        )
+}
