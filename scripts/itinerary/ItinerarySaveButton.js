a454977e7c49aa3c0b65fc17ea9a9d6d335f9ff2
nicoletatum <nicoletatum@outlook.com>
2021-02-08 13:50:13 -0600

    save itinerary button works and posts data to json file

--- scripts/itinerary/ItinerarySaveButton.js
+++ scripts/itinerary/ItinerarySaveButton.js
@@ -1,11 +1,10 @@
 import { saveItinerary } from "./ItineraryProvider.js"
-import { useEateries } from "./EateryProvider.js"
-import { useAttractions } from "./AttractionProvider.js"
-import { useParks } from './ParkProvider.js'
+import { useEateries, getEateries } from "../eateries/EateryProvider.js"
+import { useAttractions, getAttractions } from "../attractions/AttractionProvider.js"
+import { useParks, getParks } from '../parks/ParkProvider.js'
 
 const eventHub = document.querySelector(".container")
 const contentTarget = document.querySelector(".save__itenerary__button")
-
 
 export const ShowItineraryButton = () => {
     contentTarget.innerHTML = "<button id='showItinerary'> Save Itinerary! </button>"
@@ -15,18 +14,30 @@ eventHub.addEventListener("click", clickEvent => {
     if (clickEvent.target.id === "showItinerary") {
         clickEvent.preventDefault()
 
+        getAttractions()
+            .then(useAttractions)
+        getEateries()
+            .then(useEateries)
+        getParks()
+            .then(useParks)
+
         const attractionsArray = useAttractions()
+        const eateryArray = useEateries()
+        const parkArray = useParks()
+        
+        const attractionId = document.querySelector("#attractionSelect").value
+        const eateryId = document.querySelector("#eaterySelect").value
+        const parkId = document.querySelector("#parkSelect").value
+        
+
         const attractionObj = attractionsArray.find((attractionObj) => {
             return attractionObj.id === parseInt(attractionId)
         })
-        const attractionId = document.querySelector("#attractionSelect").value
 
-        const eateryId = document.querySelector("#eaterySelect").value
         const eateryObj = eateryArray.find((eateryObj) => {
             return eateryObj.id === parseInt(eateryId)
         })
 
-        const parkId = document.querySelector("#parkSelect").value
         const parkObj = parkArray.find((parkObj) => {
             return parkObj.id === parkId
         })
