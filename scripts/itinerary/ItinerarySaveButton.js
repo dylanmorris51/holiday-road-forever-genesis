4a87d74ca00ca4b86cd8ddf67f13767758ff8e35
nicoletatum <nicoletatum@outlook.com>
2021-02-08 13:11:57 -0600

    added park/event/attraction to itinerary event listener

--- scripts/itinerary/ItinerarySaveButton.js
+++ scripts/itinerary/ItinerarySaveButton.js
@@ -1,3 +1,8 @@
+import { saveItinerary } from "./ItineraryProvider.js"
+import { useEateries } from "./EateryProvider.js"
+import { useAttractions } from "./AttractionProvider.js"
+import { useParks } from './ParkProvider.js'
+
 const eventHub = document.querySelector(".container")
 const contentTarget = document.querySelector(".save__itenerary__button")
 
@@ -5,3 +10,34 @@ const contentTarget = document.querySelector(".save__itenerary__button")
 export const ShowItineraryButton = () => {
     contentTarget.innerHTML = "<button id='showItinerary'> Save Itinerary! </button>"
 }
+
+eventHub.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "showItinerary") {
+        clickEvent.preventDefault()
+
+        const attractionsArray = useAttractions()
+        const attractionObj = attractionsArray.find((attractionObj) => {
+            return attractionObj.id === parseInt(attractionId)
+        })
+        const attractionId = document.querySelector("#attractionSelect").value
+
+        const eateryId = document.querySelector("#eaterySelect").value
+        const eateryObj = eateryArray.find((eateryObj) => {
+            return eateryObj.id === parseInt(eateryId)
+        })
+
+        const parkId = document.querySelector("#parkSelect").value
+        const parkObj = parkArray.find((parkObj) => {
+            return parkObj.id === parkId
+        })
+
+    
+        const newItinerary = {
+        "park": parkObj.fullName,
+        "eatery": eateryObj.businessName,
+        "attraction": attractionObj.name,
+        }
+        
+        saveItinerary(newItinerary)
+    }
+})
--- scripts/itinerary/ItinerarySelect.js
+++ scripts/itinerary/ItinerarySelect.js
@@ -1,23 +1,23 @@
 import { saveItinerary } from "./ItineraryProvider.js"
 
 // const contentTarget = document.querySelector(".itinerary__list")
-const eventHub = document.querySelector(".container")
+// const eventHub = document.querySelector(".container")
 
-
-eventHub.addEventListener("click", clickEvent => {
-    if (clickEvent.target.id === "showItinerary") {
-        clickEvent.preventDefault()
-        const attraction = document.getElementById("itinerary__attraction").value
-        const eatery = document.getElementById(".itinerary__eatery").value
-        const park = document.getElementById("itinerary__park").value
-      // Make a new object representation of a note
-        const newItinerary = {
-        // Key/value pairs here
-        "park": park,
-        "eatery": eatery,
-        "attraction": attraction,
-        }
-      // Change API state and application state
-        saveItinerary(newItinerary)
-    }
-})
+ //COME BACK TO THIS 
+// eventHub.addEventListener("click", clickEvent => {
+//     if (clickEvent.target.id === "showItinerary") {
+//         clickEvent.preventDefault()
+//         const attraction = document.getElementById("itinerary__attraction").value
+//         const eatery = document.getElementById(".itinerary__eatery").value
+//         const park = document.getElementById("itinerary__park").value
+//       // Make a new object representation of a note
+//         const newItinerary = {
+//         // Key/value pairs here
+//         "park": park,
+//         "eatery": eatery,
+//         "attraction": attraction,
+//         }
+//       // Change API state and application state
+//         saveItinerary(newItinerary)
+//     }
+// })
