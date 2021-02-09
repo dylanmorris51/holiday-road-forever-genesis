b5094a2695f530f2003756a61240e563c28f0d98
emilycentko <emily.centko@gmail.com>
2021-02-08 23:32:53 -0600

    Edited weather icon/url

--- scripts/weather/Weather.js
+++ scripts/weather/Weather.js
@@ -3,7 +3,7 @@ export const Weather = (weatherObj) => {
     return `
     <div class="weather__card">
         <h4 class="dayOfTheWeek">${weatherObj.dt_txt}</h4>
-        <div><img class="weather__icon" src="http://openweathermap.org/img/wn/${weatherObj.weather[0].icon}@2x.png"></div>
+        <div><img class="weather__icon" src="http://openweathermap.org/img/w/${weatherObj.weather[0].icon}.png"></div>
         <div class="weather__temp">Min: ${weatherObj.main.temp_min}° / Max: ${weatherObj.main.temp_max}°</div>
         <div class="weather__forecast">${weatherObj.weather[0].description}</div>
     </div>
